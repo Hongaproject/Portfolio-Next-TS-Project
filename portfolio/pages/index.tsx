df80baa1bce0feaa4b4582d58a9b27f8ed0a5fcf
Hongaproject <tkwlscjq99@gmail.com>
2023-06-02 10:28:09 +0900

    reset

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,9 +1,6 @@
 // import Title from './Title'
 import styles from '@/styles/Home.module.css'
-
-
 import Head from "next/head"
-
 
 export default function Home() {
 
@@ -13,22 +10,7 @@ export default function Home() {
       <Head>
         <title>Profile | 소개 글</title>
       </Head>
-      <div >
-        <h1>메인 입니다.</h1>
-        <div className={styles.profileM}>
-          <div className={styles.img}>
-            이미지 부분
-            <div className={styles.content}>
-              <div className={styles.title}>
-                <b>이름 <span>영문</span></b>
-              </div>
-              <div className={styles.intro}>
-                <span>내용</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h1>메인입니다</h1>
     </div>
   )
 }
