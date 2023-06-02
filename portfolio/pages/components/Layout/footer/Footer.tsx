951df46d6ad67b6d2efd066bb03d8cd4737832ff
Hongaproject <tkwlscjq99@gmail.com>
2023-06-03 00:28:07 +0900

    new file

--- portfolio/pages/components/Layout/footer/Footer.tsx
+++ portfolio/pages/components/Layout/footer/Footer.tsx
@@ -1,9 +1,23 @@
-import styles from '@/styles/Footer.module.css'
 
 export default function Footer() {
+    
     return(    
-        <div className={styles.Footer}>
-            푸터입니다.
+        <div>
+            <footer className="text-gray-600 body-font">
+                <div className="bg-gray-100">
+                    <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                        <span className="ml-3 text-xl">Tailblocks</span>
+                    </a>
+                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 Tailblocks —
+                        {/* <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a> */}
+                    </p>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,8 +1,9 @@
 // import Title from './Title'
-import styles from '@/styles/Home.module.css'
 import Head from "next/head"
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  
 
   return (
     <div>
@@ -10,7 +11,28 @@ export default function Home() {
       <Head>
         <title>Profile | 소개 글</title>
       </Head>
-      <h1>메인입니다</h1>
+      <h1 className="text-3xl font-bold">
+        안녕하세요 홍성원 입니다.
+      </h1>
+      <h1 className="text-3xl font-bold">
+        소개글 
+      </h1>
+      <div className={styles.content}>
+        <div>
+          <img src="/photo.jpg" alt="이력서 사진" width="200px"/>
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold">이름</h1>
+          <span>자기소개</span>
+          <div>
+            <h2 className="text-2xl font-bold">Skiils</h2>
+            <span>HTML, CSS, JavaScript, React, TypeScript, Next</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.projectB}>
+        <button className="inline-flex items-center bg-purple-300 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button</button>
+      </div>
     </div>
   )
 }
