64a4c77a940d47c2209160c612f631656bf90d9b
Hongaproject <tkwlscjq99@gmail.com>
2023-06-10 11:24:39 +0900

    notion

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'www.notion.so',
+      'images.unsplash.com',
+      's3.us-west-2.amazonaws.com'
+    ]
+  }
 }
+
 const withImages = require('next-images');
 module.exports = withImages();
 
--- pages/components/projectItem/projectItem.tsx
+++ pages/components/projectItem/projectItem.tsx
@@ -1,4 +1,5 @@
-import { type } from "os";
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image"
 
 type Data = {
     data : any;
@@ -11,11 +12,15 @@ type Data = {
 export default function ProjectItem({data}: Data) {
 
     const title = data.properties.Name.title[0].plain_text
-
+    const description = data.properties.Description.url
     const github = data.properties.Github.url
+    const img = data.cover.file?.url || data.cover.external.url
+
     return(
         <div className="p-6 m-3 bg-slate-400 rounded-md">
+            <Image src={img} width="100%" height="60%"/>
             <h1>{title}</h1>
+            <h3>{description}</h3>
             <a href={github} target="_blank">깃허브 바로가기</a>
         </div>
     );
