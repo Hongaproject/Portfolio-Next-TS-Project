73787e0f074abc4bb68f8f49b91f85297d6d1674
Hongaproject <tkwlscjq99@gmail.com>
2024-01-16 14:50:59 +0900

    블로그 주소 변경

--- pages/components/LinkImg/Links.tsx
+++ pages/components/LinkImg/Links.tsx
@@ -5,7 +5,7 @@ export default function Links() {
         <div className="mb-8 leading-relaxed">
             <div className="flex">
                 <Link href='https://github.com/Hongaproject' target="_blank"><img width="48" height="48" src="/github.png" alt="github" title="Github"/></Link>
-                <Link href='https://honggacoding.tistory.com/' target="_blank"><img width="48" height="48" src="/velog.png" alt="blog" title="Velog"/></Link>
+                <Link href='https://velog.io/@hongga/posts' target="_blank"><img width="48" height="48" src="/velog.png" alt="blog" title="Velog"/></Link>
                 <Link href='mailto:tkwlscjq13@naver.com' target="_blank"><img width="48" height="48" src="/email.png" alt="Email" title="Email"/></Link>
             </div>
         </div>
