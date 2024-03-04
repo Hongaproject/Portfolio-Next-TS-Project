b05359621cf0e011025bea0497b2bc59ca9aa708
Hongaproject <tkwlscjq99@gmail.com>
2024-03-04 21:10:01 +0900

    npm run build실행

--- pages/project.tsx
+++ pages/project.tsx
@@ -41,6 +41,15 @@ export default function Project({projects}: Projectnames) {
                     <ProjectItemTr data={projects?.results} key={projects.results[2].id}/>
                 </div>
             </div>
+
+            {/* <div className="w-11/12 m-auto">
+                <h3 className="text-2xl mt-8 ml-4 mb-1 font-bold">메인 프로젝트</h3>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+                    {projects.results.slice(0,4).map((project: { id: React.Key | null | undefined; }) => (
+                        <ProjectItemF data={project} key={project.id}/>
+                    ))}
+                </div>
+            </div> */}
         </div>
     );
 }
